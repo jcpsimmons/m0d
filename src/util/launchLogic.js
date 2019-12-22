ed0ac4c2bb8dac532bf034145009fefe0ddb2ca6
Josh Simmons <jcpsimmons@gmail.com>
2019-12-22 10:44:18 -0800

    add more iOS audio workarounds

--- src/util/launchLogic.js
+++ src/util/launchLogic.js
@@ -3,7 +3,14 @@ const beginMedia = () => {
     '<audio controls loop autoplay style="display:none"> <source src="audio/audioFiles/trance.wav" type="audio/wav" /> </audio>';
   document.querySelector("#PlayButton").style.display = "none";
   document.querySelector("#SceneOne").style.display = "block";
-  document.querySelector("audio").play();
+
+  //   workaround for iOS audio issue
+  try {
+    document.querySelector("audio").play();
+    document.querySelector("audio").src = "audio/audioFiles/trance.wav";
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export default beginMedia;
