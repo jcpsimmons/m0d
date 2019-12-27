12a8694d939259d901c1c9c7a5060dcaaed666f3
Josh Simmons <001simmoj@pcsmac001009.local>
2019-12-27 15:18:11 -0800

    glitch fx

--- src/main.js
+++ src/main.js
@@ -1,10 +1,39 @@
-import beginMedia from "./util/launchLogic.js";
-import beginAudioSequence from "./audio/audio.js";
+import { EffectComposer } from './node_modules/three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from './node_modules/three/examples/jsm/postprocessing/RenderPass.js';
+import { GlitchPass } from './node_modules/three/examples/jsm/postprocessing/GlitchPass.js';
 
-const main = () => {
-  beginMedia();
-  beginAudioSequence();
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+
+var composer = new EffectComposer(renderer);
+
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+var renderPass = new RenderPass(scene, camera);
+composer.addPass(renderPass);
+
+var glitchPass = new GlitchPass();
+composer.addPass(glitchPass);
+
+var animate = function () {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  // renderer.render(scene, camera);
+  composer.render()
 };
 
-// necessary to go out of module scope so it responds to button click
-window.main = main;
+animate();
